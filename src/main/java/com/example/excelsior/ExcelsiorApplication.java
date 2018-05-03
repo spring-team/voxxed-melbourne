1e1eeb03a9fdcc81b2da4f9d713d60f40fd0a7a8
Josh Long <starbuxman@gmail.com>
2018-05-03 13:46:28 +1000

    to prod

--- src/main/java/com/example/excelsior/ExcelsiorApplication.java
+++ src/main/java/com/example/excelsior/ExcelsiorApplication.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.io.File;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
@@ -22,6 +23,8 @@ import java.util.stream.Stream;
 
 @SpringBootApplication
 public class ExcelsiorApplication {
+
+		private final File file = new File(".");
 
 		public static void main(String[] args) {
 				SpringApplication.run(ExcelsiorApplication.class, args);
